a6ffa5a29f2729c8e9c42d9c9945f199493ea31d
Armin <armin.ak89@gmail.com>
2021-02-28 18:26:50 -0500

    add modules

---
+++ modules/repositories/main.tf
@@ -0,0 +1,25 @@
+
+
+resource "github_repository" "this" {
+  topics = ["repo-managed-by-terraform"]
+
+  name        = var.name
+  description = var.description
+
+
+  archive_on_destroy     = true
+  delete_branch_on_merge = true
+  auto_init              = true
+  has_downloads          = true
+  has_issues             = true
+  has_projects           = true
+  has_wiki               = true
+  vulnerability_alerts   = true
+  visibility             = "public"
+}
+
+
+# resource "github_branch_protection" "default" {
+#   repository = github_repository.public.name
+#   branch = github_repository.public.default_branch
+# }
